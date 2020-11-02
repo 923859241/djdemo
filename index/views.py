25d14a0b408ff70ccbc49db0cc90e8f110bf3c35
chenjunyu3 <chenjunyu3@yy.com>
2020-11-02 16:54:48 +0800

    解码

--- index/views.py
+++ index/views.py
@@ -5,18 +5,27 @@ import json
 import os
 from django.conf import settings
 from index.feedback import SVMModelName
+import base64
 
 
 file_ = open(os.path.join(settings.BASE_DIR, "index/"+SVMModelName))
 
 def index_view(request):
     global resp
+    #解码
+    encodeData = request.GET.get('data')
+
+
     try:
+        base64_decrypt = base64.b64decode(encodeData.encode('utf-8'))
+        decodeData = str(base64_decrypt, 'utf-8')
+
+        print("BASE64解密串（UTF-8）:\n", decodeData)
         resp = {
             'code': '200',
             'message': 'success',
             'data': {
-                'default': str(dealWithStr(request.GET.get('data'))),
+                'default': str(dealWithStr( decodeData )),
             }
         }
     except Exception:
@@ -33,5 +42,6 @@ def index_view(request):
                                 reason='success',
                                 charset='utf-8')
         return response
+
 
 # Create your views here.
